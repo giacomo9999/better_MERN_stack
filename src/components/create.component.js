f65e2c691137e6cf0cb3352bd65ca39fe8e1ad0c
James Gary <gmarchesi888@gmail.com>
2018-12-29 12:33:24 -0500

    getting ready to create back end.

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,6 +1,52 @@
 import React, { Component } from "react";
 
 export default class Create extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangePersonName = this.onChangePersonName.bind(this);
+    this.onChangeBusinessName = this.onChangeBusinessName.bind(this);
+    this.onChangeGstNumber = this.onChangeGstNumber.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      person_name: "",
+      business_name: "",
+      business_gst_number: ""
+    };
+  }
+
+  onChangePersonName(e) {
+    this.setState({
+      person_name: e.target.value
+    });
+  }
+
+  onChangeBusinessName(e) {
+    this.setState({
+      business_name: e.target.value
+    });
+  }
+
+  onChangeGstNumber(e) {
+    this.setState({
+      business_gst_number: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(
+      `The values are ${this.state.person_name}, ${this.state.business_name}, ${
+        this.state.business_gst_number
+      }`
+    );
+    this.setState({
+      person_name: "",
+      business_name: "",
+      business_gst_number: ""
+    });
+  }
+
   render() {
     return (
       <div style={{ marginTop: 10 }}>
