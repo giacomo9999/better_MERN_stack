b752f90a19580ae206d453da4025511027d19fc0
James Gary <gmarchesi888@gmail.com>
2018-12-29 16:28:15 -0500

    React and Express talking - not sending info to DB for some reason.

--- src/App.js
+++ src/App.js
@@ -38,9 +38,6 @@ class App extends Component {
               </ul>
             </div>
           </nav>{" "}
-          <br />
-          <h2>Welcome to React CRUD Tutorial</h2>
-          <br />
           <Switch>
             <Route exact path="/create" component={Create} />
             <Route path="/edit/:id" component={Edit} />
--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Create extends Component {
   constructor(props) {
@@ -35,11 +36,17 @@ export default class Create extends Component {
 
   onSubmit(e) {
     e.preventDefault();
-    console.log(
-      `The values are ${this.state.person_name}, ${this.state.business_name}, ${
-        this.state.business_gst_number
-      }`
-    );
+    console.log("Submitting entry...");
+
+    const obj = {
+      person_name: this.state.person_name,
+      business_name: this.state.business_name,
+      business_gst_number: this.state.business_gst_number
+    };
+    axios
+      .post("http://localhost:4000/business/add", obj)
+      .then(res => console.log(res.data));
+
     this.setState({
       person_name: "",
       business_name: "",
@@ -51,19 +58,37 @@ export default class Create extends Component {
     return (
       <div style={{ marginTop: 10 }}>
         <h3>Add New Business</h3>
-        <form>
+        <form onSubmit={this.onSubmit}>
+
           <div className="form-group">
             <label>Add Person Name:</label>
-            <input type="text" className="form-control" />
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.person_name}
+              onChange={this.onChangePersonName}
+            />
           </div>
           <div className="form-group">
             <label>Add Business Name:</label>
-            <input type="text" className="form-control" />
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.business_name}
+              onChange={this.onChangeBusinessName}
+            />
           </div>
           <div className="form-group">
             <label>Add GST Number:</label>
-            <input type="text" className="form-control" />
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.business_gst_number}
+              onChange={this.onChangeGstNumber}
+            />
           </div>
+
+
           <div className="form-group">
             <input
               type="submit"
