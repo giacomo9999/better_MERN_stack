31457c66b66975a6bb0416a128a466f9f25133d4
James Gary <gmarchesi888@gmail.com>
2018-12-30 14:44:24 -0500

    adding EDIT functionality.

--- src/components/edit.component.js
+++ src/components/edit.component.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Edit extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangePersonName = this.onChangePersonName.bind(this);
+    this.onChangeBusinessName = this.onChangeBusinessName.bind(this);
+    this.onChangeGstNumber = this.onChangeGstNumber.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      person_name: "",
+      business_name: "",
+      business_gst_number: ""
+    };
+  }
   render() {
     return (
       <div>
